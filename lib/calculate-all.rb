7600a10f73ebb8ca174c0ff8c577096a87e996d3
Purin Kozuke <p@spacepro.be>
2019-08-21 23:26:14 +0900

    Define field alias name for prevent strange cast from #pluck

--- lib/calculate-all.rb
+++ lib/calculate-all.rb
@@ -36,7 +36,7 @@ module CalculateAll
     # Note the map(&:to_s). It is required since groupdate returns a
     # Groupdate::OrderHack instead of a string for the group_values which is not
     # accepted by ActiveRecord's pluck method.
-    sql_snippets = group_values.map(&:to_s) + functions.values
+    sql_snippets = group_values.map(&:to_s) + functions.map { |k, v| "#{v} AS #{k}" }
     # Fix DEPRECATION WARNING:
     # Dangerous query method, will be disallowed in Rails 6.0
     # using Arel.sql() to silence the warning
